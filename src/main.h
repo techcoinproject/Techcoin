e859b83e09f336d034d066efc1412bc9b059ecf8
Techcoin <me@carsencustoms.com>
2014-06-29 01:12:09 -0600

    Updated PoS Start Block
    Changed from 6000 to 5000, Techcoin should continue to function.

--- src/main.h
+++ src/main.h
@@ -38,7 +38,7 @@ static const int64_t MIN_RELAY_TX_FEE = MIN_TX_FEE;
 static const int64_t MAX_MONEY = 300000 * COIN;
 static const int64_t COIN_YEAR_REWARD = 4 * CENT; // 4% per year
 static const int64_t MAX_MINT_PROOF_OF_STAKE = 0.04 * COIN;	// 4% annual interest
-static const int MODIFIER_INTERVAL_SWITCH = 6000;
+static const int MODIFIER_INTERVAL_SWITCH = 5000;
 
 inline bool MoneyRange(int64_t nValue) { return (nValue >= 0 && nValue <= MAX_MONEY); }
 // Threshold for nLockTime: below this value it is interpreted as block number, otherwise as UNIX timestamp.
